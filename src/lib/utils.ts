3e0847440613b36d9590a99ce46c231b5de27a91
Harshit Sharma <cwd.harshit911@gmail.com>
2024-11-14 10:43:31 +0530

    feat: Add CURRENT_YEAR environment variable and update utils to retrieve the current year; enhance VSCode settings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * Retrieves the current year from the environment variable `CURRENT_YEAR`.
+ * If the environment variable is not set, it defaults to the current year based on the system date.
+ *
+ * @constant {number} currentYear - The current year.
+ */
+export const currentYear =
+  Number(process.env.CURRENT_YEAR) || new Date().getFullYear();
