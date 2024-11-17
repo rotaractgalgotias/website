5fd164de46da4ee02ae929fb2f29f61d0410caee
Harshit Sharma <cwd.harshit911@gmail.com>
2024-11-17 21:52:03 +0530

    feat: Fetch featured projects from the database in Sponsor component

--- src/app/sponsor-us/page.tsx
+++ src/app/sponsor-us/page.tsx
@@ -1,14 +1,18 @@
-
-import Hero from "./_components/Hero"
-import OurSponsors from "./_components/OurSponsors"
-import Projects from "./_components/Projects"
-export default function Sponsor() {
-    return (
-        <main>
-            <Hero />
-            <OurSponsors />
-            <Projects />
-        </main>
-    )
+import { prisma } from "@/lib/prisma";
+import Hero from "./_components/Hero";
+import OurSponsors from "./_components/OurSponsors";
+import Projects from "./_components/Projects";
+export default async function Sponsor() {
+  const projects = await prisma.event.findMany({
+    where: {
+      featured: true,
+    },
+  });
+  return (
+    <main>
+      <Hero />
+      <OurSponsors />
+      <Projects projects={projects} />
+    </main>
+  );
 }
-
