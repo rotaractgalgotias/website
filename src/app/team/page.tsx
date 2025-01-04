bb883c7a2b6a47a4cfdfee7748dbb77d5bef2f17
C-W-D-Harshit <cwd.harshit911@gmail.com>
2025-01-04 07:42:09 +0000

    fix: Update member role filtering to require all roles to match the current year

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -39,7 +39,7 @@ export default async function TeamPage() {
   const directorPromise = prisma.member.findMany({
     where: {
       roles: {
-        some: {
+        every: {
           year: {
             year: currentYear,
           },
