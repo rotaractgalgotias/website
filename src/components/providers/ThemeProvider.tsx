ba8c6938cef7863892f1a92831bf182aca50caef
VIJAY THAKUR <vijaysingh.handler@gmail.com>
2025-11-16 21:50:24 +0530

    Update: Media

--- src/components/providers/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
